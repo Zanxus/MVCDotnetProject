b4dc87c011888f06342e7248985b15850401706c
Emil frederiksen <Frederiksen_33@hotmail.com>
2019-12-06 10:50:19 +0100

    Removed DurationInMin prop from Movie

--- MVCDotnetProject/Models/Movie.cs
+++ MVCDotnetProject/Models/Movie.cs
@@ -9,6 +9,6 @@ namespace MVCDotnetProject.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int RuntimeInMin { get; set; }
+        
     }
 }
